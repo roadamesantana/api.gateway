cb559ab9b1c9a431d5a5135e8cb64e4e0ed68a18
Ronald Santana <rasantana.ex@montepio.pt>
2023-09-12 17:00:02 +0100

    Initial commit

---
+++ src/main/java/com/santana/api/gateway/RouteConfig.java
@@ -0,0 +1,21 @@
+package com.santana.api.gateway;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("users", r -> r.path("/user/**")
+                .uri("lb://user"))
+                .build();
+    }
+
+}
